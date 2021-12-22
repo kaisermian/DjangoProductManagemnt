2b66623fde384f3961e6b1488fec75558ff54ee3
Kaiser Mian Cabatingan <verzkaiser@gmail.com>
2021-12-22 15:15:59 +0800

    [PRM - 007] - CRUD and JSON Serializers

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,4 +3,8 @@ from dashboard import views
 
 urlpatterns = [
     path('', views.MainPage.as_view(), name='home'),
+    path('add/', views.MainPage.as_view(), name="add-product"),
+    path('update/<int:pk>/', views.updateProductAJAX, name='update-product'),
+    path('delete/<int:pk>/', views.deleteProduct, name='delete-product'),
+    path('show/<int:pk>/', views.showData, name='show-product'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -4,7 +4,139 @@ from dashboard.models import Product
 
 class MainPage(View):
     def get(self, request, *args, **kwargs):
-        context = {}
+        products = Product.objects.all()
+        form = ProductForm()
+        total_hardwares = products.filter(category='Hardware').count()
+        total_softwares = products.filter(category='Software').count()
+        total_devices = products.filter(category='Device').count()
+        total_products = products.count()
+
+        context = {
+            'products': products,
+            'total_hardwares': total_hardwares,
+            'total_softwares': total_softwares,
+            'total_devices': total_devices,
+            'total_products': total_products,
+            'form': form
+        }
+
         return render(request, "dashboard/home.html", context)
 
+    def post(self, request, *args, **kwargs):
+        data = json.loads(request.POST.get('data'))
+        response_data = {}
+
+        prod_name = data['name']
+        prod_price = data['price']
+        category = data['category']
+
+        print(data)
+        product = Product(name=prod_name, price=prod_price, category=category)
+        # form = ProductForm(request.POST, initial={'name': prod_name, 'price': prod_price, 'category': category})
+        # form = ProductForm(request.POST)
+        
+        if product.name:
+            product.save()
+            response_data['mode'] = 'success'
+            response_data['message'] = 'Product added successfully!'
+            
+            return HttpResponse(
+                json.dumps(response_data),
+                content_type="application/json"
+            )
+
+        # context = {'form': form}
+        response_data['mode'] = 'error'
+        response_data['message'] = 'Failed to add product'
+        return HttpResponse(
+            json.dumps(response_data),
+            content_type="application/json"
+        )
+
+def updateProductAJAX(request, pk):
+    product = Product.objects.get(id=pk)
+    if request.method == 'GET':
+        product_data = {}
+
+        product_data['name'] = product.name
+        product_data['price'] = product.price
+        product_data['category'] = product.category
+
+        return HttpResponse(
+            json.dumps(product_data),
+            content_type="application/json"
+        )
+        
+    else:
+        data = json.loads(request.POST.get("data"))
+        response_data = {}
+        product.name = data['name']
+        product.price = data['price']
+        product.category = data['category']
+
+        if product.name:
+            product.save()
+            response_data['mode'] = 'success'
+            response_data['message'] = 'Product is updated successfully!'
+            
+            return HttpResponse(
+                json.dumps(response_data),
+                content_type="application/json"
+            )
+
+    response_data['mode'] = 'error'
+    response_data['message'] = 'Error in updating product'
+    return HttpResponse(
+        json.dumps(response_data),
+        content_type="application/json"
+    )
+
+def deleteProduct(request, pk):
+    product = Product.objects.get(id=pk)
+    if request.method == 'GET':
+        response_data = {}
+
+        response_data['name'] = product.name
+        response_data['price'] = product.price
+        response_data['category'] = product.category
+
+        return HttpResponse(
+            json.dumps(response_data),
+            content_type='application/json'
+        )
+
+    elif request.method == 'POST':
+        response_data = {}
+        product.delete()
+
+        response_data['success'] = 'success'
+        response_data['message'] = 'Product has been removed successfully!'
+
+        return HttpResponse(
+            json.dumps(response_data),
+            content_type="application/json"
+        )
+
+def showData(request, pk):
+    if request.method == 'GET':
+        product = Product.objects.get(id=pk)
+
+        response_data = {}
+
+        response_data['name'] = product.name
+        response_data['price'] = product.price
+        response_data['category'] = product.category
+        response_data['date_created'] = str(product.date_created.strftime("%d %B, %Y %I:%M %p"))
+
+        print(response_data)
+        serializer = ProductSerializer(product)
+
+        return HttpResponse(
+            json.dumps(response_data),
+            content_type='application/json'
+        )
+
+        
+
+    
 
